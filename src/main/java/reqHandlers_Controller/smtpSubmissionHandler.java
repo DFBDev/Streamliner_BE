9ed625027615498d652498f710e3fb7a44625028
DFBDev <browndon962@gmail.com>
2024-03-16 20:56:27 -0500

    Moved smtpSubmissionHandler to handlers

--- src/main/java/reqHandlers_Controller/smtpSubmissionHandler.java
+++ src/main/java/reqHandlers_Controller/smtpSubmissionHandler.java
@@ -1,6 +1,8 @@
-package smtpHandlers;
+package reqHandlers_Controller;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import smtpHandlers.smtpSubPayloadProcessor;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
